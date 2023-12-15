b4a094a839e61b035f0c537af9366f3647a0eea0
ForeverFerret <chinanetysj@gmail.com>
2023-12-15 15:35:18 +0800

    Create 迭代查找列表中最小值和最大值.py
    https://www.liaoxuefeng.com/wiki/1016959663602400/1017316949097888

---
+++ Python/迭代查找列表中最小值和最大值.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: YuShengjia
+Email: chinanetysj@gmail.com
+"""
+
+def findMinAndMax(L):
+    """使用迭代查找一个list中最小和最大值"""
+    minNum = maxNum = None
+    for i in L:
+        if (minNum is None) and (maxNum is None):
+            minNum = maxNum = i
+            continue
+        if minNum > i:
+            minNum = i
+        if maxNum < i:
+            maxNum = i
+    return minNum, maxNum
+
+
+def main():
+    print(findMinAndMax([7, 1, 3, 9, 5]))
+
+
+if __name__ == "__main__":
+    main()
